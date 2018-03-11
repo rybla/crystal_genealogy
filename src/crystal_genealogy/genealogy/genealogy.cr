426deb2b8647d7899ea6c4e59448d7175a609080
Henry <henriiiiiiiiiii@gmail.com>
2018-03-11 12:02:40 -0700

    member updates

--- src/crystal_genealogy/genealogy/genealogy.cr
+++ src/crystal_genealogy/genealogy/genealogy.cr
@@ -5,11 +5,17 @@ alias Chromosome = Array(Gene)
 
 # A single member of a genealogy.
 struct Member
+  # the collection of genes for this member.
   property chromosome : Chromosome
+  # the members that were created subsequently as offspring, where this member was at least on of the parents.
   property children : Array(Member)
 
   def initialize
     @children = [] of Member
     @chromosome = [] of Gene
+  end
+
+  def gene(index : Int32) : Gene
+    @chromosome[index]
   end
 end
