67b91c31550f66848b27409cca7ebc45c98339c2
Henry <henriiiiiiiiiii@gmail.com>
2018-03-11 11:56:39 -0700

    init

---
+++ src/crystal_genealogy.cr
@@ -0,0 +1,6 @@
+require "./crystal_genealogy/*"
+
+# TODO: Write documentation for `CrystalGenealogy`
+module CrystalGenealogy
+  # TODO: Put your code here
+end
---
+++ src/crystal_genealogy/version.cr
@@ -0,0 +1,3 @@
+module CrystalGenealogy
+  VERSION = "0.1.0"
+end
